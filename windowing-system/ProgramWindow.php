a536ac1a3aa7ff35fdcd78e3cb4b7cce24bbb093
Milad Barzideh <milad.barzideh@snapp.cab>
2023-09-16 16:50:18 +0330

    feat: add track windowing-system exercise

---
+++ windowing-system/ProgramWindow.php
@@ -0,0 +1,29 @@
+<?php
+
+class ProgramWindow
+{
+    public $x;
+    public $y;
+    public $width;
+    public $height;
+
+    public function __construct()
+    {
+        $this->x = 0;
+        $this->y = 0;
+        $this->width = 800;
+        $this->height = 600;
+    }
+
+    public function resize($size)
+    {
+        $this->height = $size->height;
+        $this->width = $size->width;
+    }
+
+    public function move($position)
+    {
+        $this->x = $position->x;
+        $this->y = $position->y;
+    }
+}
